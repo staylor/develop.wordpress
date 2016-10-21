8ee509873aa525df4d7d326eae84e9caf411e55c
Scott Taylor <scott.c.taylor@mac.com>
2016-10-21 14:27:09 -0400

    trackback cleanup #37

--- src/wp-trackback.php
+++ src/wp-trackback.php
@@ -8,12 +8,25 @@
  * @subpackage Trackbacks
  */
 
-if (empty($wp)) {
+if ( empty( $app ) ) {
+	// load WordPress
 	require_once( __DIR__ . '/wp-load.php' );
-	wp( array( 'tb' => '1' ) );
+
+	wp( [ 'tb' => '1' ] );
 }
 
-$wpdb = $app['db'];
+$charset = $_post->get( 'charset', '' );
+if ( $charset ) {
+	$charset = str_replace( [ ',', ' ' ], '', strtoupper( trim( $charset ) ) );
+} else {
+	$charset = 'ASCII, UTF-8, ISO-8859-1, JIS, EUC-JP, SJIS';
+}
+// No valid uses for UTF-7.
+if ( false !== strpos( $charset, 'UTF-7' ) ) {
+	exit();
+}
+
+$blog_charset = get_option( 'blog_charset' );
 /**
  * Response to a trackback.
  *
@@ -25,77 +38,64 @@ $wpdb = $app['db'];
  *                              Default '0'. Accepts '0' or '1', true or false.
  * @param string $error_message Error message if an error occurred.
  */
-$trackback_response = function ($error = 0, $error_message = '') {
-	header('Content-Type: text/xml; charset=' . get_option('blog_charset') );
-	if ($error) {
-		echo '<?xml version="1.0" encoding="utf-8"?'.">\n";
-		echo "<response>\n";
-		echo "<error>1</error>\n";
-		echo '<message>' . $error_message . "</message>\n";
-		echo '</response>';
-		die();
-	} else {
-		echo '<?xml version="1.0" encoding="utf-8"?'.">\n";
-		echo "<response>\n";
-		echo "<error>0</error>\n";
-		echo '</response>';
-	}
+$trackback_response = function ( $error = 0, $error_message = '' ) use (
+	$app,
+	$response,
+	$blog_charset
+) {
+	$response->setCharset( $blog_charset );
+	$response->headers->set( 'Content-Type', 'text/xml' );
+
+	$xml = $app['mustache']->render( 'trackback/response', [
+		'error' => $error ? $error_message : ''
+	] );
+
+	$response->setContent( $xml );
+	$response->send();
 };
 
-// Trackback is done by a POST.
-$request_array = 'HTTP_POST_VARS';
-
+$tb_id = null;
 if ( ! $_get->get( 'tb_id' ) ) {
-	$tb_id = explode( '/', $app['request.uri'] );
-	$tb_id = intval( $tb_id[ count($tb_id) - 1 ] );
+	$parts = explode( '/', $app['request.uri'] );
+	$tb_id = (int) end( $parts );
+}
+
+if ( is_single() || is_page() ) {
+	$tb_id = (int) $posts[0]->ID;
+}
+
+if ( ! $tb_id ) {
+	$trackback_response( 1, __( 'I really need an ID for this to work.' ) );
+	exit();
 }
 
 $tb_url  = $_post->get( 'url', '' );
-$charset = $_post->get( 'charset', '' );
 
 // These three are stripslashed here so they can be properly escaped after mb_convert_encoding().
-$title     = wp_unslash( $_post->get( 'title', '' ) );
-$excerpt   = wp_unslash( $_post->get( 'excerpt', '' ) );
-$blog_name = wp_unslash( $_post->get( 'blog_name', '' ) );
-
-if ($charset) {
-	$charset = str_replace( array(',', ' '), '', strtoupper( trim($charset) ) );
-} else {
-	$charset = 'ASCII, UTF-8, ISO-8859-1, JIS, EUC-JP, SJIS';
-}
-
-// No valid uses for UTF-7.
-if ( false !== strpos($charset, 'UTF-7') ) {
-	die;
-}
+$_title = wp_unslash( $_post->get( 'title', '' ) );
+$_excerpt = wp_unslash( $_post->get( 'excerpt', '' ) );
+$_blog_name = wp_unslash( $_post->get( 'blog_name', '' ) );
 
 // For international trackbacks.
-if ( function_exists('mb_convert_encoding') ) {
-	$title     = mb_convert_encoding($title, get_option('blog_charset'), $charset);
-	$excerpt   = mb_convert_encoding($excerpt, get_option('blog_charset'), $charset);
-	$blog_name = mb_convert_encoding($blog_name, get_option('blog_charset'), $charset);
+if ( function_exists( 'mb_convert_encoding' ) ) {
+	$_title = mb_convert_encoding( $_title, $blog_charset, $charset );
+	$_excerpt = mb_convert_encoding( $_excerpt, $blog_charset, $charset );
+	$_blog_name = mb_convert_encoding( $_blog_name, $blog_charset, $charset );
 }
 
 // Now that mb_convert_encoding() has been given a swing, we need to escape these three.
-$title     = wp_slash($title);
-$excerpt   = wp_slash($excerpt);
-$blog_name = wp_slash($blog_name);
+$title = wp_slash( $_title );
+$excerpt = wp_slash( $_excerpt );
+$blog_name = wp_slash( $_blog_name );
 
-if ( is_single() || is_page() ) {
-	$tb_id = $posts[0]->ID;
-}
-
-if ( !isset($tb_id) || !intval( $tb_id ) ) {
-	trackback_response( 1, __( 'I really need an ID for this to work.' ) );
-}
-
-if (empty($title) && empty($tb_url) && empty($blog_name)) {
+if ( empty( $title ) && empty( $tb_url ) && empty( $blog_name ) ) {
 	// If it doesn't look like a trackback at all.
-	wp_redirect(get_permalink($tb_id));
-	exit;
+	$location = get_permalink( $tb_id );
+	wp_redirect( $location );
+	exit();
 }
 
-if ( !empty($tb_url) && !empty($title) ) {
+if ( ! empty( $tb_url ) && ! empty( $title ) ) {
 	/**
 	 * Fires before the trackback is added to a post.
 	 *
@@ -110,31 +110,33 @@ if ( !empty($tb_url) && !empty($title) ) {
 	 */
 	do_action( 'pre_trackback_post', $tb_id, $tb_url, $charset, $title, $excerpt, $blog_name );
 
-	header('Content-Type: text/xml; charset=' . get_option('blog_charset') );
-
-	if ( !pings_open($tb_id) ) {
-			trackback_response( 1, __( 'Sorry, trackbacks are closed for this item.' ) );
+	if ( ! pings_open( $tb_id ) ) {
+		$trackback_response( 1, __( 'Sorry, trackbacks are closed for this item.' ) );
+		exit();
 	}
 
-	$title =  wp_html_excerpt( $title, 250, '&#8230;' );
-	$excerpt = wp_html_excerpt( $excerpt, 252, '&#8230;' );
-
-	$comment_post_ID = (int) $tb_id;
-	$comment_author = $blog_name;
-	$comment_author_email = '';
-	$comment_author_url = $tb_url;
-	$comment_content = sprintf( "<strong>%s</strong>\n\n%s", $title, $excerpt );
-	$comment_type = 'trackback';
-
+	$wpdb = $app['db'];
 	$sql = 'SELECT * FROM ' . $wpdb->comments . ' WHERE comment_post_ID = %d AND comment_author_url = %s';
-	$dupe = $wpdb->get_results( $wpdb->prepare( $sql, $comment_post_ID, $comment_author_url) );
+	$dupe = $wpdb->get_results( $wpdb->prepare( $sql, $tb_id, $tb_url ) );
 	if ( $dupe ) {
-		trackback_response( 1, __( 'We already have a ping from that URL for this post.' ) );
+		$trackback_response( 1, __( 'We already have a ping from that URL for this post.' ) );
+		exit();
 	}
-	$commentdata = compact('comment_post_ID', 'comment_author', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_type');
 
-	wp_new_comment($commentdata);
-	$trackback_id = $wpdb->insert_id;
+	$commentdata = [
+		'comment_post_ID' => $tb_id,
+		'comment_author' => $blog_name,
+		'comment_author_email' => '',
+		'comment_author_url' => $tb_url,
+		'comment_content' => sprintf(
+			"<strong>%s</strong>\n\n%s",
+			wp_html_excerpt( $title, 250, '&#8230;' ),
+			wp_html_excerpt( $excerpt, 252, '&#8230;' )
+		),
+		'comment_type' => 'trackback',
+	];
+
+	$trackback_id = wp_new_comment( $commentdata );
 
 	/**
 	 * Fires after a trackback is added to a post.
@@ -144,5 +146,6 @@ if ( !empty($tb_url) && !empty($title) ) {
 	 * @param int $trackback_id Trackback ID.
 	 */
 	do_action( 'trackback_post', $trackback_id );
-	trackback_response( 0 );
+	$trackback_response( 0 );
+	exit();
 }
